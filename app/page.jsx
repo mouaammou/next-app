f920690cc5b8f0d84e9772d15b3aec263388b05d
mouaammou <mouadamassine@gmail.com>
2024-05-03 13:34:31 +0100

    configure finally the tailwindCss

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">Discover & share</h1>
-      <br className="max-md:hidder" />
-      <span className="capitalize">AI-Powered prompts</span>
+      <br className="md:hidden" />
+      <span className="capitalize md:hidden">AI-Powered prompts</span>
     </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx,mdx}',
-      './components/**/*.{js,ts,jsx,tsx,mdx}',
-      './app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          satoshi: ['Satoshi', 'sans-serif'],
-          inter: ['Inter', 'sans-serif'],
-        }, 
-        colors: {
-            'primary-orange': '#FF5722',
-        }
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    //screens: set sm, md, lg, xl, 2xl
+    screens: {
+      //set sm to max-width: 640px
+      sm: "640px",
+      //set md to max-width: 768px
+      md: "768px",
+      //set lg to max-width: 1024px
+      lg: "1024px",
+      //set xl to max-width: 1280px
+      xl: "1280px",
+      //set sm to max-width: 640px
+      sm: { max: "640px" },
+      //set md to max-width: 768px
+      md: { max: "768px" },
+      //set lg to max-width: 1024px
+      lg: { max: "1024px" },
+      //set xl to max-width: 1280px
+      xl: { max: "1280px" }
+    },
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        "primary-orange": "#FF5722",
       },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
